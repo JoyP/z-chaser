8b2b28ef2566a9487845cbf1422e6bf7d876988a
Dave Boling <kadowki@gmail.com>
2014-10-27 14:57:04 -0500

    Added burgers

--- public/level1.js
+++ public/level1.js
@@ -3,6 +3,8 @@ var Level1 = function(){};
 
 Level1.prototype.preload = function(){
 	game.load.image('platforms', 'assets/platform.png');
+  game.load.image('hamburger', 'assets/hamburger.jpg');
+  game.load.image('hotdog', 'assets/hotdog.png');
 };
 
 var player;
@@ -11,19 +13,46 @@ Level1.prototype.create= function(){
   this.platforms = game.add.group();
   this.platforms.enableBody = true;
   
-  this.platforms.create(0, 0, 'platforms', 0, true);
 
-  this.platforms.create();
+  var ground = this.platforms.create(0, game.world.height - 24, 'platforms');
+  ground.scale.setTo(2, 2);
+  ground.body.immovable = true;
+
   //create player group
 
-  //create platforms group
+  //enemies
+  this.placeEnemies();
+
 
 };
 
 Level1.prototype.update =function(){
-
+  game.physics.arcade.collide(this.enemies, this.platforms);
 };
 
 Level1.prototype.nextLevel = function(){
   game.state.start('level2');
 };
+
+Level1.prototype.placeEnemies = function(){
+  this.enemies = game.add.group();
+  this.enemies.enableBody = true;
+
+
+  //create 8 burgers
+  for (var i = 0; i < 8; i++){
+      var r = Math.floor(Math.random() * 599) + 1;
+      //  Create a star inside of the 'stars' group
+      var hamburger = this.enemies.create(i * 70, r + 25, 'hamburger');
+
+      //  Let gravity do its thing
+      hamburger.body.gravity.y = 300;
+  }
+
+  console.log(this.enemies);
+
+}
+
+Level1.prototype.getBurger = function(){
+
+}
