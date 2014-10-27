e59497429f62a409e9e3de965bc026766a0653f9
Matt Keef <mkeef1@gmail.com>
2014-10-27 14:26:22 -0500

    ledges added

--- public/level1.js
+++ public/level1.js
@@ -3,13 +3,23 @@ var Level1 = function(){};
 
 Level1.prototype.preload = function(){
 	game.load.image('platforms', 'assets/platform.png');
+	game.load.image('background', 'assets/sky4.png');
+
 };
 
 var player;
 
 Level1.prototype.create= function(){
+  game.add.sprite(0, 0, 'background');
   this.platforms = game.add.group();
   this.platforms.enableBody = true;
+  
+  var ground = this.platforms.create(0, game.world.height - 24, 'platforms');
+  var ledge  = this.platforms.create();
+  ground.body.immovable = true;
+  ground.scale.setTo(2, 2);
+
+  ledge = this.platforms.create(game.world.width / 2, 300, 'platforms');
   
   this.platforms.create(0, 0, 'platforms', 0, true);
 
