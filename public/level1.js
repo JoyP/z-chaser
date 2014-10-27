9b119cc991161ccbb3e6f76e30e45bbc98f17471
Matt Keef <mkeef1@gmail.com>
2014-10-27 13:35:23 -0500

    platforms

--- public/level1.js
+++ public/level1.js
@@ -2,14 +2,16 @@
 var Level1 = function(){};
 
 Level1.prototype.preload = function(){
-
+	platforms.preload();
 };
 
 var player;
-var platforms;
+var platforms = new Platforms();
 
 Level1.prototype.create= function(){
   console.log('level1 created');
+
+  platforms.create();
   //create player group
 
   //create platforms group
